1397418a2082c8db571b8bd4e44f0c15d544e20e
Noel Barrios <noel.barrios@optum.com>
2019-03-09 21:02:14 -0600

    create custom button renderer

---
+++ CustomRendererProject.iOS/RoundedButtonRendereriOS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoreGraphics;
+using CustomRendererProject;
+using CustomRendererProject.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(RoundedButton), typeof(RoundedButtonRendereriOS))]
+namespace CustomRendererProject.iOS
+{
+    public class RoundedButtonRendereriOS : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Layer.CornerRadius = 20;
+                Control.Layer.BorderWidth = 3f;
+                Control.Layer.BorderColor = Color.DarkOliveGreen.ToCGColor();
+                Control.Layer.BackgroundColor = Color.LightGray.ToCGColor();
+
+                //Control.LeftView = new UIKit.UIView(new CGRect(0, 0, 10, 0));
+                //Control.LeftViewMode = UIKit.UITextFieldViewMode.Always;
+            }
+        }
+    }
+}
